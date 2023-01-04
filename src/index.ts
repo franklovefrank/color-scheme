74bedb1bb726c007da0f2859640e75b103abcf48
franklovefrank <acfields11@gmail.com>
2023-01-04 17:51:07 -0500

    oauth flow

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,5 @@
 import express, { Request, Response } from 'express';
 import * as dotenv from 'dotenv';
-import http from "http"
-import https from "https"
-import fs from 'fs'
 import { WebClient } from '@slack/web-api';
 
 dotenv.config();
@@ -10,18 +7,39 @@ dotenv.config();
 const app = express();
 const client = new WebClient();
 
-const credentials = {
-    key: fs.readFileSync('./localhost-key.pem'),
-    cert: fs.readFileSync('./localhost.pem')
-};
-
-const httpServer = http.createServer(app);
-const httpsServer = https.createServer(credentials, app);
-
-httpServer.listen(3000, () => console.log('http app is listening on port 3000.'));
-httpsServer.listen(3443, () => console.log('https app is listening on port 3443.'));
+app.listen(3000, () => { console.log('Express server listening on port 3000 in %s mode', app.settings.env); });
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Yeehaw! Express and node are up and running');
 });
 
+app.get('/auth/slack', async (req: Request, res: Response) => {
+    const redirect = 'https://87b0-69-123-91-54.ngrok.io/auth/slack/callback';
+    const scopes = 'identity.basic,identity.email';
+    const url = `https://slack.com/oauth/v2/authorize?client_id=${process.env.SLACK_CLIENT_ID}&user_scope=${scopes}&redirect_uri=${redirect}`;
+    res.status(200)
+        .header('Content-Type', 'text/html; charset=utf-8')
+        .send(`
+            <html><body>
+            <a href="${url}"><img alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcSet="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" /></a>
+            </body></html>
+        `);
+});
+
+app.get('/auth/slack/callback', async (req: Request, res: Response) => {
+    try {
+        const response = await client.oauth.v2.access({
+            client_id: process.env.SLACK_CLIENT_ID,
+            client_secret: process.env.SLACK_CLIENT_SECRET,
+            code: (req.query as any).code 
+        });
+
+        const identity = await client.users.identity({
+            token: response.authed_user.access_token
+        });
+        res.status(200).send(`<html><body><p>Success! You are logged in with your slack account. Here are some details:</p><p>Response: ${JSON.stringify(response)}</p><p>Identity: ${JSON.stringify(identity)}</p></body></html>`);
+    } catch (err) {
+        console.log(err);
+        res.status(500).send(`<html><body><p>Sorry! Something went wrong</p><p>${JSON.stringify(err)}</p>`);
+    }
+});
